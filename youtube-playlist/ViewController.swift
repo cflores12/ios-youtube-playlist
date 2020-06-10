6f9c7ec273e7f0708dce7919fb9c8b95ff2c8664
chanales <floreschanales282@gmail.com>
2020-06-10 15:43:15 -0400

    initial xcode project creation

---
+++ youtube-playlist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-playlist
+//
+//  Created by chanales flores on 6/10/20.
+//  Copyright © 2020 chanales flores. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
