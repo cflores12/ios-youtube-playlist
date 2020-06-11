26cde62f4c01313e3ef436fbea94204579d4cc71
chanales <floreschanales282@gmail.com>
2020-06-11 17:27:13 -0400

    start of view

--- youtube-playlist/Model.swift
+++ youtube-playlist/Model.swift
@@ -8,7 +8,13 @@
 
 import Foundation
 
+protocol ModelDelegate {
+    func videosFetched( videos:[Video])
+}
+
 class Model {
+    
+    var delegate: ModelDelegate?
     
     func getVideos() {
         
@@ -37,7 +43,14 @@ class Model {
                            
                 let response = try decoder.decode(Response.self, from: data!)
                 
-                dump(response)
+                if response.items != nil {
+                    
+                    DispatchQueue.main.async {
+                        // Call the "videosReturned" method of the delegate
+                        self.delegate?.videosFetched(videos: response.items!)
+                    }
+                }
+                //dump(response)
             }
             catch {
                 
